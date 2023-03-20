7bd286e2ecc5acb228423e6d5dade93085aee8b5
mattwarne <100770327+mattwarne@users.noreply.github.com>
2023-03-20 17:32:15 +0800

    Added taks template

--- script.js
+++ script.js
@@ -3,11 +3,14 @@
 var addTaskButton = document.getElementById("add-task");
 var newTaskInput = document.getElementById("task-input");
 var todoListContainer = document.getElementById("todo-list");
+var templateContainer = document.getElementById("list-item-template");
+var template = templateContainer.innerHTML;
 // step 2 write the behaviour
 function onAddTaskClicked(event) {
     var taskName = newTaskInput.value;
     newTaskInput = "";
-    todoListContainer.insertAdjacentHTML('afterbegin', taskName);
+    var taskHTML = template.replace("<!-- TASK_NAME -->", taskName);
+    todoListContainer.insertAdjacentHTML('afterbegin', taskHTML);
 }
 // step 3 link to event handler
-addTaskButton.addEventListener('click', onAddTaskClicked);
+addTaskButton.addEventListener('click', onAddTaskClicked);``
